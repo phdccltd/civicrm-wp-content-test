1599b5a28e36f5a8c975e6127d0bd8a4710b4d5e
chriscant <chris@phdcc.com>
2021-07-23 12:51:45 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # civicrm-wp-content-test
-Test plugin to invoke the_content filter twice which can go wrong in CiviCRM 5.39+
+
+CiviCRM 5.39 for WordPress creates an issue for some sites which is solved using 
+[phdccltd/civicrm-539-wp-content-fix](https://github.com/phdccltd/civicrm-539-wp-content-fix).
+
+For sites that don't have this issue, this plugin forces the issue to appear by calling `wp_trim_excerpt()` before the main output is returned for display.
+
+# Installation
+
+Follow [these instructions](https://www.wpbeginner.com/beginners-guide/how-to-install-wordpress-plugins-and-themes-from-github/) to download this
+plugin's zip file and then upload via Plugins -> Add new on your WordPress site.
+
+# License
+
+[MIT](LICENCE)
---
+++ civicrm-wp-content-test.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Plugin Name: CiviCRM Content test
+Plugin URI: https://github.com/phdccltd/civicrm-wp-content-test
+GitHub Plugin URI: phdccltd/civicrm-wp-content-test
+Description: Test plugin to invoke the_content filter twice which can go wrong in CiviCRM 5.39+
+Author: PHDCC
+Version: 1.0
+Author URI: https://www.phdcc.com/
+License: GPL2
+*/
+
+// This file must not accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action('wp', 'civicrm_wp_content_test_basepage_handler', 15, 1); // >10 so executed after CiviCRM
+
+function civicrm_wp_content_test_basepage_handler($wp) {
+
+  add_filter('the_content', 'civicrm_wp_content_test_basepage_render');
+
+  error_log(' TEST ');
+  $excerpt = wp_trim_excerpt();
+  error_log(' XC '.substr($excerpt,0,15).' ');
+
+}
+
+function civicrm_wp_content_test_basepage_render($html) {
+
+  error_log(' TEST_RENDER '.substr($html,0,15).' ');
+
+  return $html;
+
+}
